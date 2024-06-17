7cda1d1fc83a280e1e9ffc18fd689e4bf79b4900
Tushar Patil <tusharpatil11710@gmail.com>
2024-06-17 23:14:03 +0530

    test

---
+++ AdmissionsFeeService/src/main/java/com/example/admissionsfee/AdmissionsFeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.admissionsfee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionsFeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionsFeeServiceApplication.class, args);
+	}
+
+}
---
+++ AdmissionsFeeService/src/test/java/com/example/admissionsfee/AdmissionsFeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.admissionsfee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionsFeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
