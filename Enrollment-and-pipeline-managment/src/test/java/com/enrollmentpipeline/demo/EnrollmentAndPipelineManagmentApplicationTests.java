ea200a200774ebb33b4b77e0d2ef84a410f46bc8
Tushar Patil <tusharpatil11710@gmail.com>
2024-06-17 18:57:44 +0530

    Enrollemnt

---
+++ Enrollment-and-pipeline-managment/src/test/java/com/enrollmentpipeline/demo/EnrollmentAndPipelineManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enrollmentpipeline.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentAndPipelineManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
