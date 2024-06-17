cedb733de47a78b36c0dc2b468d130883e0386a9
Tushar Patil <tusharpatil11710@gmail.com>
2024-06-17 22:53:16 +0530

    final

---
+++ RoleFeatureService/src/main/java/com/example/rolefeature/RoleFeatureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rolefeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleFeatureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleFeatureServiceApplication.class, args);
+	}
+
+}
---
+++ RoleFeatureService/src/test/java/com/example/rolefeature/RoleFeatureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rolefeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleFeatureServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
