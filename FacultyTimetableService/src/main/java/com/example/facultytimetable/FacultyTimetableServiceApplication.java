54de13cd51fcc469fca0f0aceabaa2aca989e4e5
Tushar Patil <tusharpatil11710@gmail.com>
2024-06-17 23:01:16 +0530

    test

---
+++ FacultyTimetableService/src/main/java/com/example/facultytimetable/FacultyTimetableServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.facultytimetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyTimetableServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyTimetableServiceApplication.class, args);
+	}
+
+}
---
+++ FacultyTimetableService/src/test/java/com/example/facultytimetable/FacultyTimetableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facultytimetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyTimetableServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
