d75919d0a5aac1adfe6a4ad3418fba004be958a3
Tushar Patil <tusharpatil11710@gmail.com>
2024-06-17 22:17:58 +0530

    final

--- ActivityManagement/src/main/java/com/example/activitymanagement/ActivityManagementApplication.java
+++ ActivityManagement/src/main/java/com/example/activitymanagement/ActivityManagementApplication.java
@@ -1,4 +1,4 @@
-package com.activity.demo;
+package com.example.activitymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
