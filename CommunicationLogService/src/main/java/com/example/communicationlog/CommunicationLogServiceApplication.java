2d9e6cf01b29546ed6e78c2b44d18990328df527
Tushar Patil <tusharpatil11710@gmail.com>
2024-06-17 22:26:30 +0530

    communicationlog

---
+++ CommunicationLogService/src/main/java/com/example/communicationlog/CommunicationLogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.communicationlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationLogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationLogServiceApplication.class, args);
+	}
+
+}
---
+++ CommunicationLogService/src/test/java/com/example/communicationlog/CommunicationLogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communicationlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationLogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
