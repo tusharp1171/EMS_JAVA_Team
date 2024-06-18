c0c0e5199c6a80ccb4e1470499c8fe743aea2e16
Tushar Patil <tusharpatil11710@gmail.com>
2024-06-18 09:40:29 +0530

    ActivityManagementService

---
+++ ActivityManagementService/src/test/java/com/example/ActivityManagement/ActivityManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ActivityManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
