39658bbce2ecdf6fa5a8967345491c31f55e5676
Tushar Patil <tusharpatil11710@gmail.com>
2024-06-18 09:30:08 +0530

    final setup

--- ActivityManagement/src/main/java/com/example/activitymanagement/ActivityManagementApplication.java
+++ ActivityManagement/src/main/java/com/example/activitymanagement/ActivityManagementApplication.java
@@ -6,17 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActivityManagementApplication {
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ActivityManagementApplication.class, args);
 	}
