541e51ffa5cffd337406e33bf4f12a217713c222
Tushar Patil <tusharpatil11710@gmail.com>
2024-06-20 09:31:52 +0530

    test

--- CourseManagementService/src/main/java/com/example/coursemanagement/CourseManagementServiceApplication.java
+++ CourseManagementService/src/main/java/com/example/coursemanagement/CourseManagementServiceApplication.java
@@ -10,6 +10,7 @@ public class CourseManagementServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseManagementServiceApplication.class, args);
 		
+		
 	}
 
 }
