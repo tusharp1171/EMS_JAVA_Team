6fadde46013195d30aba1a66dc54a4b17fe6e1ea
onkarbhawar111 <onkarbhawar111@gmail.com>
2024-06-20 18:27:58 +0530

    onkar code updated

--- AdmissionsFeeService/src/main/java/com/example/admissionsfee/controllers/AdmissionController.java
+++ AdmissionsFeeService/src/main/java/com/example/admissionsfee/controllers/AdmissionController.java
@@ -13,10 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-//tushar
-
 import com.example.admissionsfee.dto.AdmissionDTO;
 import com.example.admissionsfee.service.AdmissionService;
 
