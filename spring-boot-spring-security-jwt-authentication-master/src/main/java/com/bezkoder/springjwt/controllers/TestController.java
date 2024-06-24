7efa54728007e28017149c2f69d9f2d392c2a085
tusharp1171 <tusharpatil11710@gmail.com>
2024-06-24 19:37:39 +0530

    tushar code changes controller

--- spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/controllers/TestController.java
+++ spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/controllers/TestController.java
@@ -33,8 +33,7 @@ public class TestController {
   public String userAccess() {
     return "User Content.";
     
-  }
-
+  }	
   @GetMapping("/mod")
   @PreAuthorize("hasRole('MODERATOR')")
   public String moderatorAccess() {
