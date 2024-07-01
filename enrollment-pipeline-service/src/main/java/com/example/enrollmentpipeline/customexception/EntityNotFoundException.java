861b43d1ca9565123939809c60b8a24f8d35a11c
tusharp1171 <168398091+tusharp1171@users.noreply.github.com>
2024-07-01 10:59:43 +0530

    Delete enrollment-pipeline-service directory
    deleye

--- enrollment-pipeline-service/src/main/java/com/example/enrollmentpipeline/customexception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.enrollmentpipeline.customexception;
-
-public class EntityNotFoundException extends RuntimeException {
-
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
